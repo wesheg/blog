2b49c4c4497c2d7efeeda547dd436801a6e27b17
Wes Heginbotham <wesheg@gmail.com>
2025-10-19 10:03:23 +0100

    style: add typography settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Hello</h1>
+        <h1>h1 Heading</h1>
+        <h2>h2 Heading</h2>
+        <h3>h3 Heading</h3>
+        <h4>h4 Heading</h4>
+        <h5>h5 Heading</h5>
+        <h6>h6 Heading</h6>
+        <br/>
+        <p>Regular Body Text</p>
+        <p>
+          <strong>Strong Text</strong>
+        </p>
+        <p>
+          <b>Bold Text</b>
+        </p>
+        <p>
+          <em>Emphasized Text</em>
+        </p>
+        <p>
+          <i>Italicized Text</i>
+        </p>
+
+        <p className="small-text">
+          Small Text
+        </p>
+
+        <p className="large-text">
+          Large Text
+        </p>
+        
       </main>
     </div>
   );
