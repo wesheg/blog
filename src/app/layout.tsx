585251fa8438dbb559be6ad3ab738bccb458402a
Wes Heginbotham <wesheg@gmail.com>
2025-10-19 15:57:00 +0100

    chore: add context to header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable}`}>
-        <Header />
         {children}
       </body>
     </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,32 @@
+import React from "react";
+import { JSX } from "react";
 import styles from "./header.module.css";
 import Image from "next/image";
 
-export const Header = () => {
+type HeaderProps = {
+  /** If true, renders the site title as h1 tag. 
+   * Defaults to false. */
+  isHome?: boolean;
+}
+
+export const Header = ({ isHome = false }: HeaderProps) => {
   const imgWidth = 65;
   const imgHeight = imgWidth * 1.5;
+
+  const HeaderText: React.FC<{children?: React.ReactNode}> = ({ children }) => {
+    if (isHome) {
+      return <h1 className={styles.headerTitleText}>{children}</h1>
+    }
+    return <div className={styles.headerTitleText}>{children}</div>
+  }
+
   return (
     <header className={styles.blogHeader} style={{ marginBottom: imgHeight / 2}}>
       <div className={styles.headerTitle}>
         <div style={{ width: imgWidth }}>
           <Image className={styles.headerAvatar} src="/avatars/header.svg" width={imgWidth} height={imgWidth * 1.5} alt="site logo"/>
         </div>
-        <h1><span style={{fontWeight: 200}}>wesheg&apos;s</span> blog</h1>
+        <HeaderText><span className={styles.headerTextDeemphasized}>wesheg&apos;s</span> blog</HeaderText>
       </div>
       <nav className={styles.headerNav}>
         <ul>
