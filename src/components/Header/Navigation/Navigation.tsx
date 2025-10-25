59a24b59ae2209b094404316a2f4196a170a2622
Wes Heginbotham <wesheg@gmail.com>
2025-10-25 20:59:25 +0100

    feat: add social links

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,10 +1,10 @@
 import styles from "./navigation.module.css";
-// import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Navigation = () => {
   return (
-    <nav>
+    <nav className={styles.navigationContainer}>
       <ul className={styles.navigationList}>
         <li className={styles.navigationOption}>
           <Link href="/">about me</Link>
@@ -15,9 +15,15 @@ export const Navigation = () => {
         <li className={styles.navigationOption}>
           <Link href="/">projects</Link>
         </li>
-        {/* <Image src="/social/linkedin.png" height={50} width={50} alt="LinkedIn Profile" />
-        <Image src="/social/bluesky.png" height={50} width={50} alt="LinkedIn Profile" /> */}
       </ul>
+      <div>
+        <Link href="https://www.linkedin.com/in/wes-heginbotham-cfa" className={styles.navigationSocial} target="_blank">
+          <Image src="/social/linkedin.png" height={50} width={50} alt="LinkedIn Profile" />
+        </Link>
+        <Link href="https://bsky.app/profile/wesheg.bsky.social" className={styles.navigationSocial} target="_blank">
+          <Image className={styles.navigationOption} src="/social/bluesky.png" height={50} width={50} alt="LinkedIn Profile" />
+        </Link>
+      </div>
     </nav>
   );
 }
