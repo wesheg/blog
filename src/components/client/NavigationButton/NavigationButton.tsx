7fb11099960b440a957ea60ffe9ad449c25abc1c
Wes Heginbotham <wesheg@gmail.com>
2025-10-26 13:01:28 +0000

    feat: nav button first iteration

--- src/components/client/NavigationButton/NavigationButton.tsx
+++ src/components/client/NavigationButton/NavigationButton.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { useState } from "react";
+
 import style from "./navigationButton.module.css";
 
 export const NavigationButton = () => {
+  const [open, setOpen] = useState(true);
   return (
-    <div>Hello</div>
+    <button className={style.navButtonOuter} onClick={() => setOpen((prev) => !prev)}>
+      <div className={`${style.line} ${style.topLine} ${open ? style.topLineOpen : ""}`}/>
+      <div className={`${style.line} ${style.middleLine} ${open ? style.middleLineOpen : ""}`}/>
+      <div className={`${style.line} ${style.bottomLine} ${open ? style.bottomLineOpen : ""}`}/>
+    </button>
   );
 }
