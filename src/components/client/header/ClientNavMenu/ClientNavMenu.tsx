4068b2bdca538fffbf78b176515a04c44ba54cf9
Wes Heginbotham <wesheg@gmail.com>
2025-10-26 20:09:17 +0000

    feat: add media queries

--- src/components/client/header/ClientNavMenu/ClientNavMenu.tsx
+++ src/components/client/header/ClientNavMenu/ClientNavMenu.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import styles from "./clientNavMenu.module.css";
+import Link from "next/link";
 
 export const ClientNavMenu = () => {
   return (
     <div className={styles.navContainer}>
-      <div className={styles.navItem}>Hello</div>
+      <div className={styles.navItem}>
+        <Link href="/">about me</Link>
+      </div>
       <div className={styles.navItem}>Hello</div>
     </div>
   );
--- src/components/server/ServerHeader/Navigation/Navigation.tsx
+++ src/components/server/ServerHeader/Navigation/Navigation.tsx
@@ -9,26 +9,30 @@ type NavigationProps = {
 export const Navigation = ({ mobileButton }: NavigationProps) => {
   return (
     <nav className={styles.navigationContainer}>
-      <ul className={styles.navigationList}>
-        <li className={styles.navigationOption}>
-          <Link href="/">about me</Link>
-        </li>
-        <li className={styles.navigationOption}>
-          <Link href="/">blog</Link>
-        </li>
-        <li className={styles.navigationOption}>
-          <Link href="/">projects</Link>
-        </li>
-      </ul>
-      <div>
-        <Link href="https://www.linkedin.com/in/wes-heginbotham-cfa" className={styles.navigationSocial} target="_blank">
-          <Image src="/social/linkedin.png" height={50} width={50} alt="LinkedIn Profile" />
-        </Link>
-        <Link href="https://bsky.app/profile/wesheg.bsky.social" className={styles.navigationSocial} target="_blank">
-          <Image className={styles.navigationOption} src="/social/bluesky.png" height={50} width={50} alt="LinkedIn Profile" />
-        </Link>
+      <div className={styles.serverContainer}>
+        <ul className={styles.navigationList}>
+          <li className={styles.navigationOption}>
+            <Link href="/">about me</Link>
+          </li>
+          <li className={styles.navigationOption}>
+            <Link href="/">blog</Link>
+          </li>
+          <li className={styles.navigationOption}>
+            <Link href="/">projects</Link>
+          </li>
+        </ul>
+        <div>
+          <Link href="https://www.linkedin.com/in/wes-heginbotham-cfa" className={styles.navigationSocial} target="_blank">
+            <Image src="/social/linkedin.png" height={50} width={50} alt="LinkedIn Profile" />
+          </Link>
+          <Link href="https://bsky.app/profile/wesheg.bsky.social" className={styles.navigationSocial} target="_blank">
+            <Image className={styles.navigationOption} src="/social/bluesky.png" height={50} width={50} alt="LinkedIn Profile" />
+          </Link>
+        </div>
       </div>
-      {mobileButton}
+      <div className={styles.mobileButtonContainer}>
+        {mobileButton}
+      </div>
     </nav>
   );
 }
