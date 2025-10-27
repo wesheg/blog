b2a4edad2091d24ec0c78e6adbef8583d8f01bdb
Wes Heginbotham <wesheg@gmail.com>
2025-10-27 20:28:40 +0000

    feat: add to mobile nav menu

--- src/components/client/header/ClientNavMenu/ClientNavMenu.tsx
+++ src/components/client/header/ClientNavMenu/ClientNavMenu.tsx
@@ -2,14 +2,25 @@
 
 import styles from "./clientNavMenu.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export const ClientNavMenu = () => {
   return (
     <div className={styles.navContainer}>
-      <div className={styles.navItem}>
-        <Link href="/">about me</Link>
+      <Link href="/" className={`${styles.navItemContainer} ${styles.navLink}`}>home</Link>
+      <Link href="/" className={`${styles.navItemContainer} ${styles.navLink}`}>about me</Link>
+      <Link href="/" className={`${styles.navItemContainer} ${styles.navLink}`}>blog</Link>
+      <Link href="/" className={`${styles.navItemContainer} ${styles.navLink}`}>projects</Link>
+      <div className={`${styles.navItemContainer} ${styles.socialOuter}`}>
+        <div className={styles.socialInner}>
+          <Link href="https://www.linkedin.com/in/wes-heginbotham-cfa" className={styles.navigationSocial} target="_blank">
+            <Image src="/social/linkedin.png" height={75} width={75} alt="LinkedIn Profile" />
+          </Link>
+          <Link href="https://bsky.app/profile/wesheg.bsky.social" className={styles.navigationSocial} target="_blank">
+            <Image className={styles.navigationOption} src="/social/bluesky.png" height={75} width={75} alt="LinkedIn Profile" />
+          </Link>
+        </div>
       </div>
-      <div className={styles.navItem}>Hello</div>
     </div>
   );
 }
