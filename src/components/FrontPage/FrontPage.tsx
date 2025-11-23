503da80e8107871a27d4c805cb850c4e5f7ba702
Wes Heginbotham <wesheg@gmail.com>
2025-11-23 14:32:38 +0000

    feat: first iteration of headlines

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,10 +1,12 @@
 import styles from "./frontPage.module.css";
 import { FrontPageAnimation } from "./FrontPageAnimation"
+import { FrontPageHeadlines } from "./FrontPageHeadlines/FrontPageHeadlines";
 
 export const FrontPage = () => {
   return (
     <div className={styles.outerContainer}>
       <FrontPageAnimation />
+      <FrontPageHeadlines />
     </div>
   );
 }
