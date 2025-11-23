766eb1dfd318b27257fadf1420d5eba33810bfb7
Wes Heginbotham <wesheg@gmail.com>
2025-11-23 14:17:28 +0000

    style: add initial styling

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,7 +1,10 @@
+import styles from "./frontPage.module.css";
 import { FrontPageAnimation } from "./FrontPageAnimation"
 
 export const FrontPage = () => {
   return (
-    <FrontPageAnimation />
+    <div className={styles.outerContainer}>
+      <FrontPageAnimation />
+    </div>
   );
 }
