3899d685cf39ee805f1b219446d5b4555ad1dd64
Wes Heginbotham <wesheg@gmail.com>
2025-11-23 20:50:30 +0000

    chore: reorganize files

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,12 +1,24 @@
 import styles from "./frontPage.module.css";
 import { FrontPageAnimation } from "./FrontPageAnimation";
-import { FrontPageHeadlines } from "./FrontPageHeadlines/FrontPageHeadlines";
+import type { FC } from "react";
+
+const FrontPageHeadlines: FC<{ className?: string }> = ({ className }) => {
+  return (
+    <div className={`${styles.headlinesContainer} ${className ?? ""}`}>
+      <h2>Hi, I&apos;m Wes</h2>
+      <p>I&apos;m a software engineer & investment analyst</p>
+      <p>Welcome to my website!</p>
+      <button>Read the blog</button>
+    </div>
+  );
+};
 
 export const FrontPage = () => {
   return (
     <div className={styles.outerContainer}>
+      <FrontPageHeadlines className={styles.headlinesMobile} />
       <FrontPageAnimation />
-      <FrontPageHeadlines />
+      <FrontPageHeadlines className={styles.headlinesDesktop} />
     </div>
   );
 };
