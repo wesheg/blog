3bb2eb085247307bc007337c0fd19e0f1b90ba5f
Wes Heginbotham <wesheg@gmail.com>
2026-01-05 23:12:05 +0000

    feat: add to content

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       <Header />
       <main>
         <h1>About Me</h1>
-        <h2>Hi, I&apos;m Wes.</h2>
+        <h2>Hi, I&apos;m Wes</h2>
         <p>I&apos;m a Software Engineer and former Stock Analyst</p>
         <p>
           I used to work in asset management. Now I develop software for
@@ -21,6 +21,9 @@ export default function AboutMe() {
           In my spare time, I enjoy hiking, riding motorcycles, and working on
           an ever-growing list of side projects.
         </p>
+
+        <h2>Stuff I Can Do</h2>
+        <h2>Career History</h2>
       </main>
     </>
   );
