2cfe3baac786d262405694acebba899757b28aed
Wes Heginbotham <wesheg@gmail.com>
2026-01-22 20:28:23 +0000

    feat: adjust skill badges

--- src/app/about-me/Skills/Skills.tsx
+++ src/app/about-me/Skills/Skills.tsx
@@ -10,7 +10,15 @@ export const Skills = () => {
         <SkillBadge label="TypeScript" imgSrc="/skills/TypeScript.png" />
         <SkillBadge label="React" imgSrc="/skills/React.png" />
         <SkillBadge label="C++" imgSrc="/skills/Cpp.png" />
+        <SkillBadge label="iOS" imgSrc="/skills/iOS.png" imgAlt="Apple Logo" />
+        <SkillBadge label="Android" imgSrc="/skills/Android.png" />
         <SkillBadge label="Docker" imgSrc="/skills/Docker.png" />
+        <SkillBadge
+          label="SQL Databases"
+          imgSrc="/skills/SQL.png"
+          imgAlt="PostgreSQL Logo"
+        />
+        <SkillBadge label="Figma" imgSrc="/skills/Figma.png" />
         <SkillBadge label="Amazon Web Services" imgSrc="/skills/AWS.png" />
         <SkillBadge
           label="CFA Charterholder"
@@ -21,12 +29,6 @@ export const Skills = () => {
           label="Advanced Excel"
           imgSrc="/skills/Excel.png"
           imgAlt="Excel Logo"
-        />
-        <SkillBadge label="Figma" imgSrc="/skills/Figma.png" />
-        <SkillBadge
-          label="SQL Databases"
-          imgSrc="/skills/SQL.png"
-          imgAlt="Logos of PostgreSQL, MySQL, and Microsoft SQL Server"
         />
       </div>
     </section>
