1aca95ee5f2a040e4319d23603d776b81ef4cbf9
Wes Heginbotham <wesheg@gmail.com>
2026-01-22 19:24:55 +0000

    feat: add props to skill badge component

--- src/app/about-me/Skills/SkillBadge/SkillBadge.tsx
+++ src/app/about-me/Skills/SkillBadge/SkillBadge.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
 // import styles from "./skillBadge.module.css";
 
-export const SkillBadge = () => {
-  return <div></div>;
+type SkillBadgeProps = {
+  label: string;
+  imgSrc: string;
+  imgAlt?: string;
+};
+
+export const SkillBadge = ({ label, imgSrc, imgAlt }: SkillBadgeProps) => {
+  return (
+    <figure>
+      <Image alt={imgAlt ?? `${label} Logo`} src={imgSrc} />
+      <figcaption>{label}</figcaption>
+    </figure>
+  );
 };
--- src/app/about-me/Skills/Skills.tsx
+++ src/app/about-me/Skills/Skills.tsx
@@ -1,9 +1,11 @@
 import styles from "./skills.module.css";
+// import { SkillBadge } from "./SkillBadge/SkillBadge";
 
 export const Skills = () => {
   return (
     <section className={styles.skillsSection}>
       <h2>Skills</h2>
+      <div>{/* <SkillBadge /> */}</div>
     </section>
   );
 };
