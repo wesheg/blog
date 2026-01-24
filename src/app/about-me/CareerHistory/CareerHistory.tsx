ae8bb78d49f9776e420a656ae62e37fd61618622
Wes Heginbotham <wesheg@gmail.com>
2026-01-24 16:57:14 +0000

    feat: add placeholder career cards

--- src/app/about-me/CareerHistory/CareerHistory.tsx
+++ src/app/about-me/CareerHistory/CareerHistory.tsx
@@ -16,6 +16,61 @@ export const CareerHistory = () => {
       >
         <p>Test</p>
       </CareerCard>
+      <CareerCard
+        imgSrc="/companies/Bloomberg.png"
+        imgAlt="Bloomberg LP Logo"
+        jobTitle="Senior Software Engineer - Bloomberg Indices"
+        companyName="Bloomberg Index Securities Limited"
+        location="London, U.K."
+        startDate="Feb 2024"
+        endDate="Jan 2026"
+      >
+        <p>Test</p>
+      </CareerCard>
+      <CareerCard
+        imgSrc="/companies/Bloomberg.png"
+        imgAlt="Bloomberg LP Logo"
+        jobTitle="Senior Software Engineer - BQuant Help Center"
+        companyName="Bloomberg L.P."
+        location="San Francisco, California, USA"
+        startDate="Jan 2023"
+        endDate="Feb 2024"
+      >
+        <p>Test</p>
+      </CareerCard>
+      <CareerCard
+        imgSrc="/companies/Bloomberg.png"
+        imgAlt="Bloomberg LP Logo"
+        jobTitle="Developer Advocate - BQuant"
+        companyName="Bloomberg L.P."
+        location="San Francisco, Callifornia, USA"
+        startDate="Aug 2020"
+        endDate="Jan 2023"
+      >
+        <p>Test</p>
+      </CareerCard>
+      <CareerCard
+        imgSrc="/companies/EscapeGame.png"
+        imgAlt="The Escape Game, LLC Logo"
+        jobTitle="Corporate Financial Analyst"
+        companyName="The Escape Game, LLC"
+        location="Nashville, Tennessee, USA"
+        startDate="Feb 2018"
+        endDate="Aug 2020"
+      >
+        <p>Test</p>
+      </CareerCard>
+      <CareerCard
+        imgSrc="/companies/CypressCapital.png"
+        imgAlt="Cypress Capital, LLC Logo"
+        jobTitle="Investment Research Analyst"
+        companyName="Cypress Capital, LLC"
+        location="Franklin, Tennessee, USA"
+        startDate="Jan 2015"
+        endDate="Feb 2018"
+      >
+        <p>Test</p>
+      </CareerCard>
     </section>
   );
 };
