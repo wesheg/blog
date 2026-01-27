864d31760ecaa3927a5e34cd4df8f629fa613c43
Wes Heginbotham <wesheg@gmail.com>
2026-01-27 21:14:30 +0000

    refactor: move bbg index into separate file

--- src/app/about-me/CareerHistory/CareerHistory.tsx
+++ src/app/about-me/CareerHistory/CareerHistory.tsx
@@ -1,5 +1,6 @@
 import styles from "./careerHistory.module.css";
 import { CareerCard } from "./CareerCard/CareerCard";
+import { BbgIndex } from "./cards";
 
 export const CareerHistory = () => {
   return (
@@ -17,41 +18,7 @@ export const CareerHistory = () => {
       >
         <p>Test</p>
       </CareerCard>
-      <CareerCard
-        imgSrc="/companies/Bloomberg.png"
-        imgAlt="Bloomberg LP Logo"
-        jobTitle="Senior Software Engineer - Bloomberg Indices"
-        companyName="Bloomberg Index Securities Limited (BISL)"
-        location="London, U.K."
-        startDate="Feb 2024"
-        endDate="Jan 2026"
-      >
-        <p>
-          BISL is a regulated subsidiary of Bloomberg that builds broad-market
-          and bespoke financial indices. My team built tools to ensure data
-          quality and reduce time-to-market for new index launches.
-        </p>
-        <p>
-          I was the <strong>lead full-stack engineer</strong> and{" "}
-          <strong>lead user experience designer</strong> for two internal web
-          applications used by Compliance and Product.
-        </p>
-        <ol>
-          <li>
-            <strong>Index Quality Controller (iQC)</strong> &mdash; Enables
-            Compliance to monitor and intervene in an automated system of
-            quality checks on large datasets before downstream index
-            calculation. iQC empowers users to onboard new datasets, resolve
-            quality issues, and control the use of data in index construction.
-          </li>
-          <li>
-            <strong>Index Data Catalog</strong> &mdash; A searchable, canonical
-            reference for all datasets and fields used in equity and fixed
-            income indices. Used by Product Managers for idea generation, data
-            discovery, and deeper insight into index construction.
-          </li>
-        </ol>
-      </CareerCard>
+      <BbgIndex />
       <CareerCard
         imgSrc="/companies/Bloomberg.png"
         imgAlt="Bloomberg LP Logo"
---
+++ src/app/about-me/CareerHistory/cards/BbgIndex.tsx
@@ -0,0 +1,39 @@
+import { CareerCard } from "../CareerCard/CareerCard";
+
+export const BbgIndex = () => (
+  <CareerCard
+    imgSrc="/companies/Bloomberg.png"
+    imgAlt="Bloomberg LP Logo"
+    jobTitle="Senior Software Engineer - Bloomberg Indices"
+    companyName="Bloomberg Index Securities Limited (BISL)"
+    location="London, U.K."
+    startDate="Feb 2024"
+    endDate="Jan 2026"
+  >
+    <p>
+      BISL is a regulated subsidiary of Bloomberg that builds broad-market and
+      bespoke financial indices. My team built tools to ensure data quality and
+      reduce time-to-market for new index launches.
+    </p>
+    <p>
+      I was the <strong>lead full-stack engineer</strong> and{" "}
+      <strong>lead user experience designer</strong> for two internal web
+      applications used by Compliance and Product.
+    </p>
+    <ol>
+      <li>
+        <strong>Index Quality Controller (iQC)</strong> &mdash; Enables
+        Compliance to monitor and intervene in an automated system of quality
+        checks on large datasets before downstream index calculation. iQC
+        empowers users to onboard new datasets, resolve quality issues, and
+        control the use of data in index construction.
+      </li>
+      <li>
+        <strong>Index Data Catalog</strong> &mdash; A searchable, canonical
+        reference for all datasets and fields used in equity and fixed income
+        indices. Used by Product Managers for idea generation, data discovery,
+        and deeper insight into index construction.
+      </li>
+    </ol>
+  </CareerCard>
+);
