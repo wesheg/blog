2439da9771ae70a49de24c844c58b2282c64c2e3
Wes Heginbotham <wesheg@gmail.com>
2026-03-03 22:10:12 +0000

    feat: update language

--- src/app/about-me/CareerHistory/cards/BbgDevAdv.tsx
+++ src/app/about-me/CareerHistory/cards/BbgDevAdv.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { CareerCard } from "../CareerCard/CareerCard";
 
 export const BbgDevAdv = () => (
@@ -12,24 +11,14 @@ export const BbgDevAdv = () => (
     endDate="Jan 2023"
   >
     <p>
-      <Link
-        href="https://www.bloomberg.com/professional/products/bloomberg-terminal/research/bquant"
-        target="_blank"
-      >
-        BQuant
-      </Link>{" "}
-      is Bloomberg&apos;s financial data science platform, providing
-      programmatic access to data within a cloud-based notebook environment.
-    </p>
-    <p>
       BQuant clients are quantitative financial researchers. My role was to
       support the effective use of the platform through documentation, code
       examples, and demonstration projects to showcase core capabilities.
     </p>
     <p>
-      As a Developer Advocate, I acted as a communication bridge between
-      engineers and users, helping clients better understand BQuant&apos;s tools
-      and translating external feedback into product improvements.
+      As a Developer Advocate, I was a communication bridge between engineers
+      and users, helping clients better understand BQuant&apos;s tools and
+      translating external feedback into product improvements.
     </p>
   </CareerCard>
 );
