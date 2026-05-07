cf6d8b590bbafcf2532bdaaaf68eca5942d94c68
Wes Heginbotham <wesheg@gmail.com>
2026-05-07 19:01:15 +0100

    fix: use button instead of div

--- src/components/Header/mobile/MobileNavButton/MobileNavButton.tsx
+++ src/components/Header/mobile/MobileNavButton/MobileNavButton.tsx
@@ -33,7 +33,7 @@ export const MobileNavButton = () => {
   }, []);
 
   return (
-    <div
+    <button
       className={styles.navButtonOuter}
       onClick={() => setMobileNavOpen((prev) => !prev)}
     >
@@ -49,6 +49,6 @@ export const MobileNavButton = () => {
         className={`${styles.line} ${styles.bottomLine} ${prevMobileNavOpen ? styles.bottomLineOpen : ""}`}
         ref={bottomLine}
       />
-    </div>
+    </button>
   );
 };
