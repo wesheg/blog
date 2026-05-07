f112556525905f6b280b4dcf62cac20f080a331d
Wes Heginbotham <wesheg@gmail.com>
2026-05-07 17:46:01 +0100

    style: fix about me img layout

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -18,17 +18,18 @@ export default function AboutMe() {
       <main className={styles.aboutMeMain}>
         <h1 className={styles.aboutMeHeading}>About Me</h1>
         <div className={styles.introContainer}>
-          <figure
-            className={`${styles.aboutMeFigure} ${styles.portraitFigure}`}
-          >
-            <Image
-              alt="Headshot"
-              src="/aboutMe/portrait.jpg"
-              width={950}
-              height={1192}
-              className={styles.portraitImg}
-            />
-          </figure>
+          <div className={styles.imgContainer}>
+            <figure className={styles.aboutMeFigure}>
+              <Image
+                preload
+                alt="Headshot"
+                src="/aboutMe/portrait.jpg"
+                width={950}
+                height={1192}
+                className={styles.portraitImg}
+              />
+            </figure>
+          </div>
           <div>
             <h2 style={{ margin: 0 }}>Hi, I&apos;m Wes</h2>
             <p>
@@ -66,20 +67,22 @@ export default function AboutMe() {
               Bloomberg Professional Mobile App.
             </Link>
           </p>
-          <figure
-            className={`${styles.aboutMeFigure} ${styles.conferenceFigure}`}
-          >
-            <Image
-              alt="Presenting at the Bloomberg Tech Expo in London, 2025"
-              src="/aboutMe/TechExpo.jpg"
-              width={950}
-              height={1192}
-              className={styles.conferenceImg}
-            />
-            <figcaption className="img-caption">
-              Bloomberg Tech Expo, London 2025
-            </figcaption>
-          </figure>
+          <div className={styles.imgContainer}>
+            <figure
+              className={`${styles.aboutMeFigure} ${styles.conferenceFigure}`}
+            >
+              <Image
+                alt="Presenting at the Bloomberg Tech Expo in London, 2025"
+                src="/aboutMe/TechExpo.jpg"
+                width={950}
+                height={1192}
+                className={styles.conferenceImg}
+              />
+              <figcaption className="img-caption">
+                Bloomberg Tech Expo, London 2025
+              </figcaption>
+            </figure>
+          </div>
         </div>
         <Skills />
         <CareerHistory />
