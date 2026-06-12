2ed7283100ba6f6ef417f140c8bbd4006118e154
Wes Heginbotham <wesheg@gmail.com>
2026-06-12 09:06:39 +0100

    docs: add docstrings

--- src/utils/buttonUtils.ts
+++ src/utils/buttonUtils.ts
@@ -4,8 +4,8 @@ import type { MouseEvent, TouchEvent } from "react";
  * Apply a CSS class to an event target.
  * Use this to style buttons on events like `mousedown` and `touchstart`
  *
- * @param event - MouseEvent or TouchEvent on a DOM element
- * @param className - CSS class to apply to the event target
+ * @param {React.MouseEvent | React.TouchEvent} event - MouseEvent or TouchEvent on a DOM element
+ * @param {string} className - CSS class to apply to the event target
  */
 export function pressDown(event: MouseEvent | TouchEvent, className: string) {
   (event.target as Element).classList.add(className);
@@ -16,8 +16,8 @@ export function pressDown(event: MouseEvent | TouchEvent, className: string) {
  * Use this to style buttons on events like `mouseup`, `mouseleave`,
  * `touchend` and `touchcancel`
  *
- * @param event - MouseEvent or TouchEvent on a DOM element
- * @param className - CSS class to remove from the event target
+ * @param {React.MouseEvent | React.TouchEvent} event - MouseEvent or TouchEvent on a DOM element
+ * @param {string} className - CSS class to remove from the event target
  */
 export function popUp(event: MouseEvent | TouchEvent, className: string) {
   (event.target as Element).classList.remove(className);
--- src/utils/cms.ts
+++ src/utils/cms.ts
@@ -1,3 +1,9 @@
+/**
+ * Fetch content from headless WordPress
+ *
+ * @param {string} graphQlQuery - Stringified GraphQL query
+ * @returns {T} - Specify a return type when the function is called
+ */
 export async function fetchFromCms<T>(graphQlQuery: string) {
   const response = await fetch("https://cms.wesheg.dev/graphql", {
     method: "POST",
