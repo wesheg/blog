c726e534e880935cf78379f23d77107e2d0797c5
Wes Heginbotham <wesheg@gmail.com>
2026-06-16 22:26:30 +0100

    feat: update programming languages

--- src/app/about-me/Skills/Skills.tsx
+++ src/app/about-me/Skills/Skills.tsx
@@ -4,32 +4,14 @@ import { SkillBadge } from "./SkillBadge/SkillBadge";
 export const Skills = () => {
   return (
     <section className={styles.skillsSection}>
-      <h2>Skills</h2>
+      <h2>I&apos;ve Worked In</h2>
       <div className={styles.badgeGallery}>
-        <SkillBadge label="Python" imgSrc="/skills/Python.png" />
         <SkillBadge label="TypeScript" imgSrc="/skills/TypeScript.png" />
         <SkillBadge label="React" imgSrc="/skills/React.png" />
+        <SkillBadge label="Python" imgSrc="/skills/Python.png" />
         <SkillBadge label="C++" imgSrc="/skills/Cpp.png" />
-        <SkillBadge label="iOS" imgSrc="/skills/iOS.png" imgAlt="Apple Logo" />
-        <SkillBadge label="Android" imgSrc="/skills/Android.png" />
-        <SkillBadge label="Docker" imgSrc="/skills/Docker.png" />
-        <SkillBadge
-          label="SQL Databases"
-          imgSrc="/skills/SQL.png"
-          imgAlt="PostgreSQL Logo"
-        />
-        <SkillBadge label="Figma" imgSrc="/skills/Figma.png" />
-        <SkillBadge label="Amazon Web Services" imgSrc="/skills/AWS.png" />
-        <SkillBadge
-          label="CFA Charterholder"
-          imgSrc="/skills/CFA.png"
-          imgAlt="CFA Institute Logo"
-        />
-        <SkillBadge
-          label="Financial Modeling"
-          imgSrc="/skills/Excel.png"
-          imgAlt="Excel Logo"
-        />
+        <SkillBadge label="Swift" imgSrc="/skills/Swift.png" />
+        <SkillBadge label="Kotlin" imgSrc="/skills/Kotlin.png" />
       </div>
     </section>
   );
