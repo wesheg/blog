93ad5f67714e6cc6d9c67a0bb5ec6fff2a650d41
Wes Heginbotham <wesheg@gmail.com>
2026-07-11 21:04:07 +0100

    chore: add to rollup

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { ArticleLink } from "./ArticleLink/ArticleLink";
 export { ArticleDate } from "./ArticleDate/ArticleDate";
 export { BlogArticle } from "./BlogArticle/BlogArticle";
 export { FrontPage } from "./FrontPage/FrontPage";
+export { FrontPageAnimation } from "./FrontPage/FrontPageAnimation/FrontPageAnimation";
 export { Header } from "./Header/Header";
 export { LinkAsButton } from "./LinkAsButton/LinkAsButton";
 export * from "./types";
